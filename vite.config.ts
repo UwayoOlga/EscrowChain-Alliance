3348e0040cbc47314f17d628d695620d8a6cf1fb
UwayoOlga <uwayoolga@gmail.com>
2026-01-21 22:16:33 +0200

    Server Troubleshooting

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,8 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+        react: path.resolve(__dirname, 'node_modules/react'),
+        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
       }
     },
     build: {
